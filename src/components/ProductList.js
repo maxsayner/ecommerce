ab2b3caf35748d607818260205e034d9832fd964
Maxson Sayner <max.sayner@gmail.com>
2019-05-06 20:35:55 -0600

    context api added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 import Product from './Product';
 import Title from './Title';
+import { storeProducts } from '../data';
+import { ProductConsumer } from '../Context';
 export default class ProductList extends Component {
     state = {
-        products: []
+        products: storeProducts
     }
     render() {
+        console.log(this.state.products);
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                         <Title name="our" title="Products" />
+
                         <div className="row">
+                            <ProductConsumer>
+                                {(value) => {
+                                    return <h1>{value}</h1>
+                                }}
+                            </ProductConsumer>
                         </div>
                     </div>
                 </div>
