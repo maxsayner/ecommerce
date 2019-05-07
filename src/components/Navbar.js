a1cf1d896794b15ec531b8dc05cba6fb27449b5f
Maxson Sayner <max.sayner@gmail.com>
2019-05-06 18:38:36 -0600

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5" >
+
+                <Link to='/'>
+                    <i class="fas fa-tv"></i>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            products
+                    </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus" />
+                        my cart
+                    </button>
+                </Link>
+            </nav >
         )
     }
 }
