d26d6013629a1bfc1394e9023d39c1d60994f0f7
Maxson Sayner <max.sayner@gmail.com>
2019-05-06 20:51:22 -0600

    fetched product.id

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,7 +8,7 @@ export default class ProductList extends Component {
         products: storeProducts
     }
     render() {
-        console.log(this.state.products);
+
         return (
             <React.Fragment>
                 <div className="py-5">
@@ -18,14 +18,16 @@ export default class ProductList extends Component {
                         <div className="row">
                             <ProductConsumer>
                                 {(value) => {
-                                    return <h1>{value}</h1>
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} />
+                                    })
                                 }}
                             </ProductConsumer>
                         </div>
                     </div>
                 </div>
             </React.Fragment >
-            // <Product /> 
+
         );
     }
 }
